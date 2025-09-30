c5de45435ba3291b68db1348c01ddb0451ec0bdd
Azhar Khalid <azhar.tkjzone@gmail.com>
2025-09-30 11:14:26 +0700

    feat: add product and order route registration to main server

--- cmd/main.go
+++ cmd/main.go
@@ -1,69 +1,73 @@
 package main
 
 import (
-	"log"
-	"os"
+    "log"
+    "os"
 
-	"github.com/Caknoooo/go-gin-clean-starter/middlewares"
-	"github.com/Caknoooo/go-gin-clean-starter/modules/auth"
-	"github.com/Caknoooo/go-gin-clean-starter/modules/user"
-	"github.com/Caknoooo/go-gin-clean-starter/providers"
-	"github.com/Caknoooo/go-gin-clean-starter/script"
-	"github.com/samber/do"
+    "github.com/Caknoooo/go-gin-clean-starter/middlewares"
+    "github.com/Caknoooo/go-gin-clean-starter/modules/auth"
+    "github.com/Caknoooo/go-gin-clean-starter/modules/order"
+    "github.com/Caknoooo/go-gin-clean-starter/modules/product"
+    "github.com/Caknoooo/go-gin-clean-starter/modules/user"
+    "github.com/Caknoooo/go-gin-clean-starter/providers"
+    "github.com/Caknoooo/go-gin-clean-starter/script"
+    "github.com/samber/do"
 
 	"github.com/common-nighthawk/go-figure"
 	"github.com/gin-gonic/gin"
 )
 
 func args(injector *do.Injector) bool {
-	if len(os.Args) > 1 {
-		flag := script.Commands(injector)
-		return flag
-	}
+    if len(os.Args) > 1 {
+        flag := script.Commands(injector)
+        return flag
+    }
 
-	return true
+    return true
 }
 
 func run(server *gin.Engine) {
-	server.Static("/assets", "./assets")
+    server.Static("/assets", "./assets")
 
-	port := os.Getenv("GOLANG_PORT")
-	if port == "" {
-		port = "8888"
-	}
+    port := os.Getenv("GOLANG_PORT")
+    if port == "" {
+        port = "8888"
+    }
 
-	var serve string
-	if os.Getenv("APP_ENV") == "localhost" {
-		serve = "0.0.0.0:" + port
-	} else {
-		serve = ":" + port
-	}
+    var serve string
+    if os.Getenv("APP_ENV") == "localhost" {
+        serve = "0.0.0.0:" + port
+    } else {
+        serve = ":" + port
+    }
 
-	myFigure := figure.NewColorFigure("Caknoo", "", "green", true)
-	myFigure.Print()
+    myFigure := figure.NewColorFigure("Caknoo", "", "green", true)
+    myFigure.Print()
 
-	if err := server.Run(serve); err != nil {
-		log.Fatalf("error running server: %v", err)
-	}
+    if err := server.Run(serve); err != nil {
+        log.Fatalf("error running server: %v", err)
+    }
 }
 
 func main() {
-	var (
-		injector = do.New()
-	)
+    var (
+        injector = do.New()
+    )
 
-	providers.RegisterDependencies(injector)
+    providers.RegisterDependencies(injector)
 
-	if !args(injector) {
-		return
-	}
+    if !args(injector) {
+        return
+    }
 
-	server := gin.Default()
-	server.Use(middlewares.CORSMiddleware())
+    server := gin.Default()
+    server.Use(middlewares.CORSMiddleware())
 
-	// Register module routes
-	user.RegisterRoutes(server, injector)
-	auth.RegisterRoutes(server, injector)
+    // Register module routes
+    user.RegisterRoutes(server, injector)
+    auth.RegisterRoutes(server, injector)
+    product.RegisterRoutes(server, injector)
+    order.RegisterRoutes(server, injector)
 
-	run(server)
+    run(server)
 }
