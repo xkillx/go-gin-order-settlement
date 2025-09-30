433e546961c258ca35a8d919b6779dcbbad3fe02
Azhar Khalid <azhar.tkjzone@gmail.com>
2025-09-30 11:14:15 +0700

    feat: add product and order module dependencies with repositories, services and controllers

--- providers/core.go
+++ providers/core.go
@@ -5,8 +5,14 @@ import (
 	authController "github.com/Caknoooo/go-gin-clean-starter/modules/auth/controller"
 	authRepo "github.com/Caknoooo/go-gin-clean-starter/modules/auth/repository"
 	authService "github.com/Caknoooo/go-gin-clean-starter/modules/auth/service"
+	orderController "github.com/Caknoooo/go-gin-clean-starter/modules/order/controller"
+	orderRepo "github.com/Caknoooo/go-gin-clean-starter/modules/order/repository"
+	orderService "github.com/Caknoooo/go-gin-clean-starter/modules/order/service"
+	productController "github.com/Caknoooo/go-gin-clean-starter/modules/product/controller"
+	productRepo "github.com/Caknoooo/go-gin-clean-starter/modules/product/repository"
+	productService "github.com/Caknoooo/go-gin-clean-starter/modules/product/service"
 	userController "github.com/Caknoooo/go-gin-clean-starter/modules/user/controller"
-	"github.com/Caknoooo/go-gin-clean-starter/modules/user/repository"
+	userRepo "github.com/Caknoooo/go-gin-clean-starter/modules/user/repository"
 	userService "github.com/Caknoooo/go-gin-clean-starter/modules/user/service"
 	"github.com/Caknoooo/go-gin-clean-starter/pkg/constants"
 	"github.com/samber/do"
@@ -29,11 +35,15 @@ func RegisterDependencies(injector *do.Injector) {
 	db := do.MustInvokeNamed[*gorm.DB](injector, constants.DB)
 	jwtService := do.MustInvokeNamed[authService.JWTService](injector, constants.JWTService)
 
-	userRepository := repository.NewUserRepository(db)
+	userRepository := userRepo.NewUserRepository(db)
 	refreshTokenRepository := authRepo.NewRefreshTokenRepository(db)
+	productRepository := productRepo.NewProductRepository(db)
+	orderRepository := orderRepo.NewOrderRepository(db)
 
 	userService := userService.NewUserService(userRepository, db)
 	authService := authService.NewAuthService(userRepository, refreshTokenRepository, jwtService, db)
+	productService := productService.NewProductService(productRepository, db)
+	orderService := orderService.NewOrderService(orderRepository, productRepository, db)
 
 	do.Provide(
 		injector, func(i *do.Injector) (userController.UserController, error) {
@@ -44,6 +54,18 @@ func RegisterDependencies(injector *do.Injector) {
 	do.Provide(
 		injector, func(i *do.Injector) (authController.AuthController, error) {
 			return authController.NewAuthController(i, authService), nil
+		},
+	)
+
+	do.Provide(
+		injector, func(i *do.Injector) (productController.ProductController, error) {
+			return productController.NewProductController(i, productService), nil
+		},
+	)
+
+	do.Provide(
+		injector, func(i *do.Injector) (orderController.OrderController, error) {
+			return orderController.NewOrderController(i, orderService), nil
 		},
 	)
 }
